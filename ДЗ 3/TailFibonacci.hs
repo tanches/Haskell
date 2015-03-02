ae47de4157ccd02dc72e781133725ff2c76f6a0a
tanches <ta_nya_nya11@mail.ru>
2015-03-02 23:35:41 +0400

    tail fibonacci

--- ДЗ 3/TailFibonacci.hs
+++ ДЗ 3/TailFibonacci.hs
@@ -1,11 +1,11 @@
 ﻿module Main where
 
 -- числа Фибоначчи: 0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, ...
-fibonacciTail n = go n
+fibonacciTail n = go n 1
     where
-        go 0 = 0
-        go 1 = 1
-        go 2 = 1
-        go n = go (n-1) + go (n-2)
+        go 0 k = k
+        go 1 k = k
+        go 2 k = 1
+        go n k = go (n-1) k + go (n-2) k
 
-main = putStrLn(show(fibonacciTail 3))
+main = putStrLn (show (fibonacciTail 7))
