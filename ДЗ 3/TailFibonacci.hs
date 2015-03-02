ef30703ee4c4c2c35f9e5ae712d8ecce25245771
tanches <ta_nya_nya11@mail.ru>
2015-03-02 10:20:48 +0400

    Tail Fibonacci

---
+++ ДЗ 3/TailFibonacci.hs
@@ -0,0 +1,11 @@
+﻿module Main where
+
+-- числа Фибоначчи: 0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, ...
+fibonacciTail n = go n
+    where
+        go 0 = 0
+        go 1 = 1
+        go 2 = 1
+        go n = go (n-1) + go (n-2)
+
+main = putStrLn(show(fibonacciTail 3))
