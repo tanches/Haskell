112cd43b41b9ab2732700c109854ca3e21dcb7c7
tanches <ta_nya_nya11@mail.ru>
2015-03-04 15:45:16 +0400

    Homework3

--- ДЗ 3/Binom.hs
+++ ДЗ 3/Binom.hs
@@ -13,9 +13,28 @@ binomKof k n =
     if n < k
         then error "First argument must be less than second"
     else
-	if n == k
-		then 1
-	else
-		div (factorial n) ((factorial k) * (factorial (n-k)))
+        if n == k
+            then 1
+        else
+            div (factorial n) ((factorial k) * (factorial (n-k)))
 
-main = putStrLn (show (binomKof 2 6))
+-- _________________________________________________________________________
+
+-- хвостовая рекурсия
+factorial' :: Integer -> Integer
+factorial' n = go n 1
+    where
+        go 0 k = k
+        go 1 k = k
+        go n k = go (n-1) (k*n)
+
+binomKof2 k n =
+    if n < k
+        then error "First argument must be less than second"
+    else
+        if n == k
+            then 1
+        else
+            div (factorial' n) ((factorial' k) * (factorial' (n-k)))
+
+main = putStrLn (show (binomKof2 2 5))
--- ДЗ 3/TailFibonacci.hs
+++ ДЗ 3/TailFibonacci.hs
@@ -1,11 +1,10 @@
 ﻿module Main where
 
 -- числа Фибоначчи: 0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, ...
-fibonacciTail n = go n 1
+fibonacciTail :: Integer -> Integer
+fibonacciTail n = go n (0, 1)
     where
-        go 0 k = k
-        go 1 k = k
-        go 2 k = 1
-        go n k = go (n-1) k + go (n-2) k
+        go n (m, k) | n == 0 = m
+                    | otherwise = go (n - 1) (k, m + k)
 
-main = putStrLn (show (fibonacciTail 7))
+main = putStrLn (show (fibonacciTail 9))
