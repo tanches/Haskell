b23c832b0dda702639d93b04f39698a8c4472aa6
tanches <ta_nya_nya11@mail.ru>
2015-03-09 21:43:27 +0400

    Dinner

---
+++ ДЗ 4/Dinner.hs
@@ -0,0 +1,20 @@
+﻿module Main where
+
+-- PROBLEM A
+data DinnerOrder = Chicken | Pasta | NotOrder deriving (Show)
+-- Interp. Various for dinner
+-- Examples don't need
+fn_for_dinner_order :: DinnerOrder -> a
+fn_for_dinner_order d = case d of
+                        Chicken  -> undefined
+                        Pasta    -> undefined
+                        NotOrder -> undefined
+
+-- PROBLEM B
+dinner_order_to_msg :: DinnerOrder -> String
+dinner_order_to_msg dotm = case dotm of
+                                Chicken  -> "The passenger ordered chicken."
+                                Pasta    -> "The passenger ordered pasta."
+                                NotOrder -> "The passenger didn'r order a dinner."
+
+main = putStrLn (dinner_order_to_msg Pasta)
