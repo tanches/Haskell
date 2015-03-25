2cd912153739e42dc853fd555b611f2f26d86f74
tanches <ta_nya_nya11@mail.ru>
2015-03-25 14:27:31 +0400

    Revert "BinarySearchTree"
    This reverts commit f2fe91f394071803bedad1149b730e02a4ee24fb.

--- ДЗ 5/Main.hs
+++ ДЗ 5/Main.hs
@@ -1,16 +1,6 @@
 ﻿module Main where
 
--- Создать тип данных для бинарного дерева поиска (binary search tree), хранящего значения типа Int.
 data BinarySearchTree =
                         Null
-                        | Node BinarySearchTree Int BinarySearchTree
-						deriving (Show)
--- interp. binary search tree
-						
-tree_null = Null
-tree_one = Node Null 1 Null
-tree_two = Node Null 2 tree_one
-tree_three = Node Null 3 tree_two
-
---main = putStrLn (show (height tree_three))
+                        | Node BinarySearchTree Integer BinarySearchTree
 
