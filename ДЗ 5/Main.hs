9cc0ecd17f5342cb002a077d854b331359aecabf
tanches <ta_nya_nya11@mail.ru>
2015-03-25 14:34:05 +0400

    BinarySearchTree
    added examples and function of height

--- ДЗ 5/Main.hs
+++ ДЗ 5/Main.hs
@@ -1,6 +1,23 @@
 ﻿module Main where
 
+-- Создать тип данных для бинарного дерева поиска (binary search tree), хранящего значения типа Int.
 data BinarySearchTree =
                         Null
-                        | Node BinarySearchTree Integer BinarySearchTree
+                        | Node BinarySearchTree Int BinarySearchTree
+                        deriving (Show)
+-- interp. binary search tree
 
+tree_null = Null
+tree_one = Node Null 1 Null
+tree_two = Node Null 2 tree_one
+tree_three = Node Null 3 tree_two
+
+--main = putStrLn (show tree_two)
+
+-- Реализовать функцию подсчёта высоты этого дерева.
+height :: BinarySearchTree -> Integer
+height Null = 0
+--находим максимальное дерево и к нему прибавляем 1, т.к. меньшее дерево и так войдет в большее по высоте
+height (Node left _ right) = (max (height left) (height right)) + 1
+
+main = putStrLn (show (height tree_three))
