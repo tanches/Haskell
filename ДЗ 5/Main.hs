3fbdf00838feb4dcf55e7c11a405260e2366715f
tanches <ta_nya_nya11@mail.ru>
2015-03-26 23:19:08 +0400

    binary tree
    find

--- ДЗ 5/Main.hs
+++ ДЗ 5/Main.hs
@@ -28,3 +28,15 @@ sumT Null = 0
 sumT (Node left middle right) = (+) middle ((+) (sumT left) (sumT right))
 
 main = putStrLn (show (sumT tree_three))
+
+-- Реализовать функцию поиска заданного элемента в дереве.
+find Null _ = (Null)
+find (Node left toFind right) n
+                             -- ищем справа
+                             | n > toFind = Node left toFind (find right n)
+                             -- ищем слева
+                             | n < toFind = Node (find left n) toFind right
+                             -- нашли!
+                             | n == toFind = error "Actually, it isn't error. I really found this number"
+
+main = putStrLn (show (find tree_two 2))
