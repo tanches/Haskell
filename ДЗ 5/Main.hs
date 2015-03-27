9366cc9af33d41a8d2045869234ae9f5d4bcb4d1
tanches <ta_nya_nya11@mail.ru>
2015-03-27 10:23:55 +0400

    binary search tree
    fix mistakes

--- ДЗ 5/Main.hs
+++ ДЗ 5/Main.hs
@@ -8,9 +8,9 @@ data BinarySearchTree =
 -- interp. binary search tree
 
 tree_null = Null
-tree_one = Node Null 1 Null
+tree_one = Node Null 3 Null
 tree_two = Node Null 2 tree_one
-tree_three = Node Null 3 tree_two
+tree_three = Node Null 1 tree_two
 
 --main = putStrLn (show tree_two)
 
@@ -39,4 +39,4 @@ find (Node left toFind right) n
                              -- нашли!
                              | n == toFind = error "Actually, it isn't error. I really found this number"
 
-main = putStrLn (show (find tree_two 2))
+main = putStrLn (show (find tree_three 3))
