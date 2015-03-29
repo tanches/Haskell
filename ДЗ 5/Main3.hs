e48ad6d49a5bb6b9d5fd3ff46af0613818190592
tanches <ta_nya_nya11@mail.ru>
2015-03-29 13:50:47 +0400

    examples

--- ДЗ 5/Main3.hs
+++ ДЗ 5/Main3.hs
@@ -3,6 +3,9 @@
 -- Создать тип данных для списков, в которых чередуются элементы двух разных типов.
 data ListOfDiffTypes = Null
                         | Cons String ListOfDiffTypes Int ListOfDiffTypes
-                        | Con String ListOfDiffTypes
+                        | Con String ListOfDiffTypes Int ListOfDiffTypes String ListOfDiffTypes
                         | Cons2 Int ListOfDiffTypes String ListOfDiffTypes
-                        | Con2 Int ListOfDiffTypes
+                        | Con2 Int ListOfDiffTypes String ListOfDiffTypes Int ListOfDiffTypes
+						
+ex1 = Cons "hello" 5 Con "world" 3 "!"
+ex2 = Cons2 4 "wow" 3 Con2 3 "same" 7
