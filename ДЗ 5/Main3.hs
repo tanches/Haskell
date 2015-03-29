9c3970b0b84ead8a5e6cae2d5a2e7cb06bf91a96
tanches <ta_nya_nya11@mail.ru>
2015-03-29 13:44:28 +0400

    list of diff types

---
+++ ДЗ 5/Main3.hs
@@ -0,0 +1,7 @@
+﻿module Main where
+
+data ListOfDiffTypes = Null
+                        | Cons String ListOfDiffTypes Int ListOfDiffTypes
+                        | Con String ListOfDiffTypes
+                        | Cons2 Int ListOfDiffTypes String ListOfDiffTypes
+                        | Con2 Int ListOfDiffTypes
