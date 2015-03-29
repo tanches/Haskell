dc715d5a198a64d00572568d9f53fe50ce34953f
tanches <ta_nya_nya11@mail.ru>
2015-03-29 13:45:20 +0400

    add problem

--- ДЗ 5/Main3.hs
+++ ДЗ 5/Main3.hs
@@ -1,5 +1,6 @@
 ﻿module Main where
 
+-- Создать тип данных для списков, в которых чередуются элементы двух разных типов.
 data ListOfDiffTypes = Null
                         | Cons String ListOfDiffTypes Int ListOfDiffTypes
                         | Con String ListOfDiffTypes
