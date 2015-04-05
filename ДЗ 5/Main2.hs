2639b4e42f212bc0ac72405d4afe5796f0b8635d
tanches <ta_nya_nya11@mail.ru>
2015-04-05 15:45:38 +0400

    fix mistakes

--- ДЗ 5/Main2.hs
+++ ДЗ 5/Main2.hs
@@ -3,19 +3,19 @@
 --Создать тип данных для бинарного дерева, хранящего в листьях элементы произвольного типа.
 data BinarySearchTree a =
                         Null
-                        | BinarySearchTree a (BinarySearchTree a)  (BinarySearchTree a)
+                        | Node a (BinarySearchTree a)  (BinarySearchTree a)
                         deriving (Show)
 -- interp. binary search tree with different types
 
 
 tree_null = Null
-tree_one = BinarySearchTree "Hello" Null Null
-tree_two = BinarySearchTree "World" (BinarySearchTree "!" Null Null) (tree_one)
+tree_one = Node "Hello" Null Null
+tree_two = Node "World" (Node "!" Null Null) (tree_one)
 
 --Реализовать функцию подсчёта высоты этого дерева.
 height :: BinarySearchTree a -> Int
 height Null = 0
-height (BinarySearchTree _ left right) = max (height left) (height right) + 1
+height (Node _ left right) = max (height left) (height right) + 1
 
 --main = putStrLn (show (height tree_two))
 
@@ -24,6 +24,6 @@ height (BinarySearchTree _ left right) = max (height left) (height right) + 1
 tmap :: (a -> b) -> BinarySearchTree a -> BinarySearchTree b
 tmap operation tr = case tr of
                 Null -> Null
-                BinarySearchTree l m r -> BinarySearchTree (operation l) (tmap operation m) (tmap operation r)
+                Node l m r -> Node (operation l) (tmap operation m) (tmap operation r)
 
 main = putStrLn("It's OK")
