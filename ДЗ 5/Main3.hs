ce9029f944205b06548f0f4fa2cb20bf27a8aad8
tanches <ta_nya_nya11@mail.ru>
2015-04-13 12:08:17 +0400

    task 3

--- ДЗ 5/Main3.hs
+++ ДЗ 5/Main3.hs
@@ -1,33 +1,32 @@
 ﻿module Main where
 
 -- Создать тип данных для списков, в которых чередуются элементы двух разных типов.
-data ListOfDiffTypes = Null
-                        | Cons String Int ListOfDiffTypes
-                        | Con String Int String ListOfDiffTypes
-                        | Cons2 Int String ListOfDiffTypes
-                        | Con2 Int String Int ListOfDiffTypes
-						
-ex1 = Cons "hello" 5 Null
-ex2 = Cons2 4 "wow" Null
-ex3 = Con "wow" 3 "rest" Null
-ex4 = Con "wow" 3 "rest" Null
-ex5 = Cons "hello" 5 ex3
+data ListOfDiffTypes a b = Null
+                | Twist a (ListOfDiffTypes b a)
+-- Interp. Definition of list with alternating types of elements
 
-fn_for_list :: ListOfDiffTypes -> a
+fn_for_list :: ListOfDiffTypes a b -> c
 fn_for_list list = case list of
-                    Null -> undefined
-                    Cons str int list -> undefined str (fn_for_list list)
-                    Con str int str2 list -> undefined str (fn_for_list list)
-                    Cons2 int str list -> undefined str (fn_for_list list)
-                    Con2 int str int2 list -> undefined str (fn_for_list list)
+    Null -> undefined
+    Twist b list -> undefined b (fn_for_list list)
+
+list_empty = Null
+list_one = Twist "string" list_empty
+list_two = Twist "wow" (Twist 4 list_empty)
 
 -- Реализовать функцию определения длины такого списка.
-length2 :: ListOfDiffTypes -> Int
+length2 :: ListOfDiffTypes a b -> Int
 length2 l = case l of
                     Null -> 0
-                    Cons str int l -> 2 + (length2 l)
-                    Con str int str2 l -> 3 + (length2 l)
-                    Cons2 int str l -> 2 + (length2 l)
-                    Con2 int str int2 l -> 3 + (length2 l)
+                    Twist b list -> 1 + (length2 list)
 
-main = putStrLn (show (length2 ex5))
+--main = putStrLn (show (length2 list_two))
+
+-- Реализовать функцию dmap, принимающую такой список и две функции преобразования элементов,
+-- и возвращающую новый список, содержащий преобразованные элементы в том же порядке, что и исходные.
+--dmap :: ((a b) -> (c d)) -> ((c d) -> (e f)) -> ListOfDiffTypes a b -> ListOfDiffTypes e f
+dmap operation operation2 list = case list of
+                Null -> Null
+                Twist arg list -> (operation operation2 arg) (dmap operation operation2 list)
+
+main = putStrLn ("It's OK")
